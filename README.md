f75cd71bd11a550c8f50ae3676492594047a54b5
myoichi <myoichi@vmware.com>
2024-08-16 12:14:56 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nsx-back-and-forth
+VMware NSX official documents have a pull-down to select a version, but the 3.x series documentation does not show the 4.x series version, and vice versa.
+This extension adds a link to the latest version of another system (4.2 for 3.x, 3.2 for 4.x) to each document in the pull-down for convenience in document comparison.
+
+## install
+As this extension is not packaged, please load it in Developer Mode to use it:
+1. Open chrome://extensions/ in Chrome
+2. Toggle "Developer mode" ON in the top right corner of the screen
+3. Click "Load unpacked" in the top left corner and select the "nsx-back-and-forth" folder
---
+++ nsx.js
@@ -0,0 +1,25 @@
+//Drop Downメニューを取得
+dropdown = document.getElementsByClassName("dropdown-menu version-list-wrapper");
+if(dropdown && dropdown.length >= 1){
+    dropdown = dropdown[0];
+
+    //追加の選択肢を作成
+    newversion = document.createElement("a");
+    newversion.className = "dropdown-item version-item px-3 py-1";
+
+    //NSX 3.x系の場合は4.2のリンクを追加
+    current_url = window.location.href;
+    pattern = /VMware-NSX-T-Data-Center\/[0-9]\.[0-9]\//;
+    if(current_url.match(pattern)){
+        newversion.href = current_url.replace(pattern, "VMware-NSX/4.2/");
+        text = document.createTextNode("VMware NSX 4.2 *");
+    }
+    //NSX 4.x系の場合は3.2のリンクを追加
+    pattern = /VMware-NSX\/[0-9]\.[0-9]\//;
+    if(current_url.match(pattern)){
+        newversion.href = current_url.replace(pattern, "VMware-NSX-T-Data-Center/3.2/");
+        text = document.createTextNode("VMware NSX T Data Center 3.2 *");
+    }
+    newversion.appendChild(text);
+    dropdown.appendChild(newversion);
+}
