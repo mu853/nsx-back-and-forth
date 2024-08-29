f664157e02932e508b5bba9f0966f71deaf00796
mu853 <mu.youichi853@gmail.com>
2024-08-29 11:40:02 +0900

    add function to automatically change language settings to match the URL

--- nsx.js
+++ nsx.js
@@ -23,3 +23,19 @@ if(dropdown && dropdown.length >= 1){
     newversion.appendChild(text);
     dropdown.appendChild(newversion);
 }
+
+spans = document.getElementById("localeDropDownMenuButton").getElementsByTagName("span");
+current_locale = "";
+for(let i = 0; i < spans.length; i++){
+    if(spans[i].classList.contains("currentLocale")){
+        current_locale = spans[i];
+        break;
+    }
+}
+
+if(current_locale){
+    matches = window.location.href.match(/\/([a-w]+)\/VMware-NSX/);
+    if(matches){
+        current_locale.textContent = matches[1];
+    }
+}
