bfa67cd733c99fcb17645e6c97ff6bb445e1fdc5
mu853 <mu.youichi853@gmail.com>
2024-09-20 05:54:55 +0900

    add lacking versions

--- nsx.js
+++ nsx.js
@@ -1,29 +1,35 @@
+function create_newversion(dropdown, href, text){
+    //追加のDrop Down選択肢を作成
+    newversion = document.createElement("a");
+    newversion.className = "dropdown-item version-item px-3 py-1";
+    newversion.href = href;
+    newversion.appendChild(document.createTextNode(text));
+    dropdown.appendChild(newversion);
+}
+
 //Drop Downメニューを取得
 dropdown = document.getElementsByClassName("dropdown-menu version-list-wrapper");
 if(dropdown && dropdown.length >= 1){
     dropdown = dropdown[0];
-
-    //追加の選択肢を作成
-    newversion = document.createElement("a");
-    newversion.className = "dropdown-item version-item px-3 py-1";
-
-    //NSX 3.x系の場合は4.2のリンクを追加
     current_url = window.location.href;
+ 
+    //NSX 3.x系の場合は4.2のリンクを追加
     pattern = /VMware-NSX-T-Data-Center\/[0-9]\.[0-9]\//;
     if(current_url.match(pattern)){
-        newversion.href = current_url.replace(pattern, "VMware-NSX/4.2/");
-        text = document.createTextNode("VMware NSX 4.2 *");
+        create_newversion(dropdown, current_url.replace(pattern, "VMware-NSX/4.0/"), "VMware NSX 4.0 *");
+        create_newversion(dropdown, current_url.replace(pattern, "VMware-NSX/4.1/"), "VMware NSX 4.1 *");
+        create_newversion(dropdown, current_url.replace(pattern, "VMware-NSX/4.2/"), "VMware NSX 4.2 *");
     }
+
     //NSX 4.x系の場合は3.2のリンクを追加
     pattern = /VMware-NSX\/[0-9]\.[0-9]\//;
     if(current_url.match(pattern)){
-        newversion.href = current_url.replace(pattern, "VMware-NSX-T-Data-Center/3.2/");
-        text = document.createTextNode("VMware NSX T Data Center 3.2 *");
+        create_newversion(dropdown, current_url.replace(pattern, "VMware-NSX-T-Data-Center/3.1/"), "VMware NSX T Data Center 3.1 *");
+        create_newversion(dropdown, current_url.replace(pattern, "VMware-NSX-T-Data-Center/3.2/"), "VMware NSX T Data Center 3.2 *");
     }
-    newversion.appendChild(text);
-    dropdown.appendChild(newversion);
 }
 
+//現在の表示言語を取得
 spans = document.getElementById("localeDropDownMenuButton").getElementsByTagName("span");
 current_locale = "";
 for(let i = 0; i < spans.length; i++){
@@ -33,6 +39,7 @@ for(let i = 0; i < spans.length; i++){
     }
 }
 
+//URLに合わせて表示言語を修正
 if(current_locale){
     matches = window.location.href.match(/\/([a-w]+)\/VMware-NSX/);
     if(matches){
